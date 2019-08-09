4e8577e3debd62737aa587139de286a6b9f961df
degrecci <henrique.degrecci@upx.com>
2019-08-09 13:18:27 -0300

    :sparkles: Ends navigate-between-pages

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,7 +1,12 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-}
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about" title="About Page">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index;
